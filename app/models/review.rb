96cd2169904440f34300e1ad66c14c5d0c2239ad
yuriirogulya <yuriirogulya@gmail.com>
2018-05-03 20:43:12 +0300

    delete readme

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   
   validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
+  validate :custom_validate
+
+  def custom_validate
+    
+  end
+
 end
