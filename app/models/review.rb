8fd1f75a5709086a1e9c8692dbb0e191aaf54d43
yuriirogulya <yuriirogulya@gmail.com>
2018-05-04 20:19:53 +0300

    refactored custom validation methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,18 +3,26 @@ class Review < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   
   validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
-  validate :custom_validate
+  validate :cannot_review_item_you_never_booked, :cannot_review_user_you_never_dealt
 
-  def custom_validate
-    if reviewable_type.equal?('Item')
+  def cannot_review_item_you_never_booked
+    if reviewable_type == 'Item'
       unless user.bookings.where(item_id: reviewable_id).exists?
         errors.add(:review, 'you never booked this item')
       end
-    elsif reviewable_type.equal?('User')
-      unless Booking.joins(:item).where('items.owner_id' => reviewable_id, 'renter_id' => user.id).exists?
+    end
+  end
+
+  def cannot_review_user_you_never_dealt
+    if reviewable_type == 'User'
+      unless Booking.joins(:item)
+        .where('items.owner_id' => reviewable_id, 'renter_id' => author_id)
+        .or(Booking.joins(:item)
+        .where('items.owner_id' => author_id, 'renter_id' => reviewable_id)).exists?
         errors.add(:review, 'you never dealt with this user')
       end
     end
   end
+
 end
 
