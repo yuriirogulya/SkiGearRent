ba96bafc5dd6b40c182aed255c5cac3724f91ca5
yuriirogulya <yuriirogulya@gmail.com>
2018-05-05 10:29:27 +0300

    fixed review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :reviewable, polymorphic: true
   belongs_to :user, foreign_key: 'author_id'
-  
-  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5}
-  validate :cannot_review_item_you_never_booked, :cannot_review_user_you_never_dealt
+  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5 }
+  validate :cannot_review_item_you_never_booked, :cannot_review_user_you_never_dealt, on: :create
 
   def cannot_review_item_you_never_booked
     if reviewable_type == 'Item'
@@ -16,13 +15,11 @@ class Review < ApplicationRecord
   def cannot_review_user_you_never_dealt
     if reviewable_type == 'User'
       unless Booking.joins(:item)
-        .where('items.owner_id' => reviewable_id, 'renter_id' => author_id)
-        .or(Booking.joins(:item)
-        .where('items.owner_id' => author_id, 'renter_id' => reviewable_id)).exists?
+                    .where('items.owner_id' => reviewable_id, 'renter_id' => author_id)
+                    .or(Booking.joins(:item)
+                    .where('items.owner_id' => author_id, 'renter_id' => reviewable_id)).exists?
         errors.add(:review, 'you never dealt with this user')
       end
     end
   end
-
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,10 @@ class User < ApplicationRecord
   belongs_to :city
   has_many :items, foreign_key: 'owner_id'
   has_many :bookings, foreign_key: 'renter_id'
-  has_many :reviews, through: :items, as: :reviewable, foreign_key: 'author_id'
+  has_many :reviews, as: :reviewable
+  has_many :item_reviews, through: :items, foreign_key: :author_id, source: :reviews
+  has_many :written_reviews, foreign_key: :author_id, class_name: 'Review'
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :username, uniqueness: { case_sensitive: false }, format: { without: /\s/ }
 end
-
-
-
-
