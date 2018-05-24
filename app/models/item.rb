ac3179b5043e979ca08c4e8d43a47efa2a81be76
yuriirogulya <yuriirogulya@gmail.com>
2018-05-24 14:03:56 +0300

    added scope to filter items by non booked date range

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,21 @@ class Item < ApplicationRecord
   scope :with_name,     -> (name)     { where('name ilike ?', "%#{name}%") }
   scope :with_category, -> (category) { joins(:category).where('category_id = ?',  category) }
   scope :with_option,   -> (option)   { joins(:items_options).where('option_id = ?', option) }
+
   scope :with_price, -> (min, max, days) do
-    t = arel_table
-    total_price = t[:daily_price] * days
-    where(total_price.gteq(min).and(total_price.lteq(max)))
+    i = arel_table
+    total_price = i[:daily_price] * days
+    where(total_price.in(min..max)) #переписати на IN range min..max
+  end
+
+  scope :booked, -> (start_date, end_date) do
+    b = Booking.arel_table
+    joins(:bookings).where(b[:start_date].lteq(end_date).and(b[:end_date].gteq(start_date)))
+  end
+
+  scope :available, -> (start_date, end_date) do
+    i = arel_table
+    booked_items = booked(start_date, end_date).ids
+    where(i[:id].not_in(booked_items))
   end
 end
