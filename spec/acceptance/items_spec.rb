1838ae1228a2dd9f17642781706f02d6f8a9eb3c
yuriirogulya <yuriirogulya@gmail.com>
2018-05-30 10:37:14 +0300

    fixed api tests to use parameter function

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Items' do
-  header 'Accept', 'application/json'
-  header 'Content-Type', 'application/json'
 
     get '/items' do
       let!(:items) { create_list(:item, 5) }
@@ -14,64 +12,84 @@ resource 'Items' do
       end
     end
 
-    get '/items?name=hello' do
+    get '/items' do
+      parameter :name, 'Item name'
       let!(:item1) { create(:item, name: 'someitem') }
       let!(:item2) { create(:item, name: 'hello world') }
+      let(:name) { 'hello' }
       example_request 'Getting items with cetrain word in name' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(1)
       end
     end
 
-    get '/items?category=1' do
-      let!(:category1) { create(:category, id: 1) }
-      let!(:category2) { create(:category, id: 2) }
+    get '/items' do
+      parameter :category, 'Category id'
+      let!(:category1) { create(:category) }
+      let!(:category2) { create(:category) }
       let!(:item1) { create(:item, category: category1) }
       let!(:item2) { create(:item, category: category2) }
+      let(:category) { category1.id }
       example_request 'Getting items with certain category' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(1)
       end
     end
 
-    get '/items?options[]=3' do
+    get '/items' do
+      parameter 'options[]', 'Item options'
       let!(:option1) { create(:option, id: 1) }
       let!(:option2) { create(:option, id: 2) }
       let!(:option3) { create(:option, id: 3) }
       let!(:item1) { create(:item, options: [option2]) }
       let!(:item2) { create(:item, options: [option1, option3]) }
       let!(:item3) { create(:item, options: [option2, option3]) }
+      let('options[]') { option3.id }
       example_request 'Getting items with certain options' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(2)
       end
     end
 
-    get '/items?price[min]=500&price[max]=1000' do
+    get '/items' do
+      parameter 'price[min]', 'MIN price range'
+      parameter 'price[max]', 'MAX price range'
       let!(:item1) { create(:item, daily_price: 500) }
       let!(:item2) { create(:item, daily_price: 1500) }
       let!(:item3) { create(:item, daily_price: 1000) }
+      let('price[min]') { 500 }
+      let('price[max]') { 1000 }
       example_request 'Getting items with certain price range' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(2)
       end
     end
 
-    get '/items?price[min]=5000&price[max]=6000&price[days]=5' do
+    get '/items' do
+      parameter 'price[min]', 'MIN price range'
+      parameter 'price[max]', 'MAX price range'
+      parameter 'price[days]', 'Booking day count'
       let!(:item1) { create(:item, daily_price: 500) }
       let!(:item2) { create(:item, daily_price: 1500) }
       let!(:item3) { create(:item, daily_price: 1000) }
+      let('price[min]') { 2000 }
+      let('price[max]') { 4000 }
+      let('price[days]') { 5 }
       example_request 'Getting items with certain price range and days number' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(1)
       end
     end
     
-    get '/items?available[start_date]=2018-06-16&available[end_date]=2018-06-20' do
+    get '/items' do
+      parameter 'available[start_date]', 'Start date to find available bookings'
+      parameter 'available[end_date]', 'End date to find available bookings'
       let!(:item1) { create(:item) }
       let!(:item2) { create(:item) }
       let!(:booking1) { create(:booking, item: item1, start_date:'2018-06-10', end_date: '2018-06-20') }
       let!(:booking2) { create(:booking, item: item2, start_date:'2018-06-10', end_date: '2018-06-15') }
+      let('available[start_date]') { '2018-06-16' }
+      let('available[end_date]') { '2018-06-20' }
       example_request 'Getting available items by time range' do
         json = JSON.parse(response_body)
         expect(json.count).to eq(1)
@@ -94,19 +112,23 @@ resource 'Items' do
       end
     end
   
-    post "/items?name=item_name&owner=user" do
+    post "/items" do
+      parameter :name, 'New item name'
+      parameter :owner, 'New item owner'
       let!(:category) { create(:category) }
       let!(:item) { create(:item) }
-      let(:item_name) { item.name }
       let(:user) { create(:user) }
+      let(:name) { item.name }
+      let(:owner) { user.id }
       example_request 'Getting item create action request' do    
         expect { Item.create(name: :item_name, owner: user, category: category) }.to change{ Item.count }.by(1)
       end
     end
 
-    post "/items?description=item_description" do
+    post "/items" do
+      parameter :description, 'New item description'
       let!(:item) { create(:item)}
-      let(:item_description) { item.description }
+      let(:description) { item.description }
       example_request 'Getting item create action request without required params' do
         expect(status).to eq(422)
       end
